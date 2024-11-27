966aaf2a2a5290f53a672c2a606734a4902ad81f
Danmecpy <danmecpy@gmail.com>
2024-11-27 14:45:27 -0300

    Update.3.0

--- code_button.ino
+++ code_button.ino
@@ -1,16 +1,19 @@
+//Botões de controle 
 
 
 #define b_frente 5
 #define b_tras 4
 #define b_direita 12
-#define b_esquerda 7pinMode(b_parar,INPUT);
+#define b_esquerda 7
 #define b_parar 0
 
+// Portas do motor 1 
 #define motor1_rpwm 11
 #define motor1_lpwm 10
 #define motor1_re 1
 #define motor1_le 2
 
+// Portas do motor 2
 #define motor2_rpwm 3
 #define motor2_lpwm 6
 #define motor2_re 9
@@ -37,12 +40,14 @@ void setup() {
   pinMode(b_esquerda,INPUT);
   pinMode(b_parar,INPUT);
 
+    // Definindo os botões como alto logo no início para economizar fio
   digitalWrite(b_frente,HIGH);
   digitalWrite(b_tras,HIGH);
   digitalWrite(b_direita,HIGH);
   digitalWrite(b_esquerda,HIGH);
   digitalWrite(b_parar,HIGH);
   
+  // Começa os motores sempre parados caso o sistema reinicie
   pararMotores();
 
   
@@ -50,7 +55,7 @@ void setup() {
 
 
 void loop() {
-  
+  // Variáveis com os valores do estado dos botões
   int frente = digitalRead(b_frente);
   int tras = digitalRead(b_tras);
   int direita = digitalRead(b_direita);
@@ -60,21 +65,21 @@ void loop() {
   //Serial.println(tras); // branco do lado esquerdo
   //Serial.println(direita); // azul do lado direito
   //Serial.println(esquerda); // branco do lado direito
-  
+  // condições realizadas pelos botões 
   if (frente==0){
-    moverFrente(80);
+    moverFrente(95);
   }
 
   else if (tras==0){
-    moverTras(80);
+    moverTras(95);
   }
 
   else if (direita==0){
-    moverDireita(80);
+    moverDireita(95);
   }
 
   else if (esquerda==0){
-    moverEsquerda(80);
+    moverEsquerda(95);
   }
   else if (digitalRead(b_parar)==0){
     pararMotores();
@@ -113,7 +118,7 @@ void moverTras(int velocidade) {
   digitalWrite(motor2_le, HIGH);
   digitalWrite(motor2_re, HIGH);
 }
-// MOTOR 1 DIREITA , MOTOR 2 ESQUERDA
+// Mover para esquerda e direita
 
 void moverEsquerda(int velocidade) {
   analogWrite(motor1_rpwm, LOW);
